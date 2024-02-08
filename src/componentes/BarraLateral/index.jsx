b43541067ca5f58bc6be5cdef99698f4c9f82a9e
PedroPassos081 <pedro.passos081@gmail.com>
2024-02-08 12:40:27 -0300

    Adicionando o header do projeto e iniciando a navbar

---
+++ src/componentes/BarraLateral/index.jsx
@@ -0,0 +1,33 @@
+import styled from "styled-components"
+
+const ListaEstilizada = styled.ul`
+    list-style: none;
+    padding: 0;
+    margin: 0;
+    width: 212px
+`
+
+const BarraLateral = () => {
+    return (
+        <aside>
+            <nav>
+                <ListaEstilizada>
+                <ul>
+                    <li>
+                        <a href="/">
+                            Inicio
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/">
+                            Mais curtidos
+                        </a>
+                    </li>
+                </ul>
+                </ListaEstilizada>
+            </nav>
+        </aside>
+    )
+}
+
+export default BarraLateral
