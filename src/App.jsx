1719b7a7c7c944ea4d814209ef71b867f03602c3
PedroPassos081 <pedro.passos081@gmail.com>
2024-02-12 16:28:20 -0300

    Adicionando a "galeria" do projeto

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,13 @@ import Banner from "./componentes/Banner"
 import bannerBackground from "./assets/banner.png"
 import Galeria from "./componentes/Galeria"
 
+import fotos from './fotos.json'
+import { useState } from "react"
 
 const FundoGradiente = styled.div`
   background: linear-gradient(174.61deg, #041833 4.16%, #04244F 48%, #154580 96.76%);
   width: 100%;
-  height: 100vh;
+  min-height: 100vh;
 `
 const AppContainer = styled.div`
   width: 1440px;
@@ -27,7 +29,8 @@ const ConteudoGaleria = styled.section`
   flex-grow: 1;
 `
 
-function App() {
+const App = () => {
+  const [fotosDaGaleria, setFotosDaGaleria] = useState(fotos)
 
   return (
     <FundoGradiente>
@@ -41,7 +44,7 @@ function App() {
               texto="A galeria mais completa de fotos do espaço!"
               backgroundImage={bannerBackground}
             />
-            <Galeria />
+            <Galeria fotos={fotosDaGaleria} />
           </ConteudoGaleria>
         </ContainerMain>
       </AppContainer>
